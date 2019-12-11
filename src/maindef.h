f3dfa2bfdf4f24f8d113d3b40ffc51683722c701
Ralic Lo <raliclo@gmail.com>
2019-12-12 04:53:31 +0800

    Updated on 2018-Oct-07

--- src/maindef.h
+++ src/maindef.h
@@ -1,7 +1,7 @@
 /****************************  maindef.h   **********************************
 * Author:        Agner Fog
 * Date created:  2006-08-26
-* Last modified: 2018-08-15
+* Last modified: 2018-10-08
 * Project:       objconv
 * Module:        maindef.h
 * Description:
@@ -13,7 +13,7 @@
 #define MAINDEF_H
 
 // Program version
-#define OBJCONV_VERSION         2.50
+#define OBJCONV_VERSION         2.51
 
 
 // Integer type definitions with platform-independent sizes:
